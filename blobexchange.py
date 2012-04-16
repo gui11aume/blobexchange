3f9ac690760c19e969cb309e99bb329fe69e11b1
Guillaume Filion <guillaume.filion@gmail.com>
2012-04-16 02:06:54 +0200

    Set blob key name to SHA1 digest, but fucked up CSS a bit...

--- blobexchange.py
+++ blobexchange.py
@@ -2,8 +2,9 @@
 
 
 import os
-import urllib
 import datetime
+
+from hashlib import sha1
 
 from google.appengine.ext import db
 from google.appengine.ext import blobstore
@@ -15,10 +16,8 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 class BlobRef(db.Model):
    """Datastore entry for blobs."""
-   blob_key = blobstore.BlobReferenceProperty(
-                  blobstore.BlobKey,
-                  required = True
-              )
+   blob_key = blobstore.BlobReferenceProperty(blobstore.BlobKey)
+   creation_time = db.TimeProperty(auto_now_add=True)
 
 
 class MainHandler(webapp.RequestHandler):
@@ -55,16 +54,20 @@ class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
 
       # NB: 'get_uploads' is a list of 'BlobInfo' objects.
       blob_info = self.get_uploads('file')[0]
+      # Set the SHA1 digest of the file as key name.
+      # This is unique, not ridiculously long and contains
+      # only URL characters.
+      sha1_digest = sha1(blob_info.filename).hexdigest()
+
       blob_ref = BlobRef(
-          key_name = blob_info.filename,
-          blob_key = blob_info.key()
+         key_name = sha1_digest,
+         blob_key = blob_info.key()
       )
       blob_ref.put()
 
       # First fill in the content itself (not the HTML page).
       content_values = {
-         # 'blob_link': blob_info.key(),
-         'blob_link': blob_info.filename
+         'blob_link': sha1_digest,
       }
 
       content_path = os.path.join(dot, 'content', 'link_content.html')
@@ -83,28 +86,39 @@ class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
       )
 
 
-class NewHandler(blobstore_handlers.BlobstoreDownloadHandler):
+class DownloadHandler(blobstore_handlers.BlobstoreDownloadHandler):
    def get(self, path):
       blob = BlobRef.get_by_key_name(path)
       self.send_blob(blob.blob_key, save_as=True)
 
 
+class ByBlobKeyHandler(blobstore_handlers.BlobstoreDownloadHandler):
+   def get(self, path):
+      blob_info = blobstore.BlobInfo.get(path)
+      self.send_blob(blob_info, save_as=True)
+
+
 class DeleteHandler(blobstore_handlers.BlobstoreDownloadHandler):
    def get(self):
-      three_days_ago = datetime.datetime.today() \
+      two_weeks_ago = datetime.datetime.today() \
             + datetime.timedelta(days=-15)
       blob_count = blobstore.BlobInfo.all().count()
       all_blobs_query = blobstore.BlobInfo.all().fetch(blob_count)
       for blob in all_blobs_query:
-         if blob.creation < three_days_ago:
+         if blob.creation < two_weeks_ago:
             blob.delete()
+      all_BlobRef_query = BlobRef.all().fetch(blob_count)
+      for blob_ref in all_BlobRef_query:
+         if blob_ref.creation_time < two_weeks_ago:
+            blob_ref.delete()
 
 
 def main():
    application = webapp.WSGIApplication(
         [('/', MainHandler),
          ('/upload', UploadHandler),
-         ('/download/([^/]+)?', NewHandler),
+         ('/download/([^/]+)?', DownloadHandler),
+         ('/blobkey/([^/]+)?', ByBlobKeyHandler),
          ('/delete', DeleteHandler),
         ], debug=True)
    run_wsgi_app(application)
